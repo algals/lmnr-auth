611e9c10dcb6c249dba3430e8564ace1d7a25301
angliukas <algirdas.alisauskas@gmail.com>
2018-11-04 22:47:45 +0200

    up

--- src/main/java/lt/luminor/auth/dao/entity/ContactEntity.java
+++ src/main/java/lt/luminor/auth/dao/entity/ContactEntity.java
@@ -18,6 +18,8 @@ public class ContactEntity {
     private String email;
     @Column
     private String phone;
+    @Column
+    private String address;
 
     public Long getId() {
         return id;
@@ -53,5 +55,13 @@ public class ContactEntity {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
