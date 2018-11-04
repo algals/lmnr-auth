4cd13498fdae0e56217b662dadac3656a73b498c
angliukas <algirdas.alisauskas@gmail.com>
2018-11-04 16:35:56 +0200

    up

--- src/main/java/lt/luminor/auth/controller/MainController.java
+++ src/main/java/lt/luminor/auth/controller/MainController.java
@@ -9,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
-import java.security.Principal;
-
 
 @Controller
 public class MainController {
